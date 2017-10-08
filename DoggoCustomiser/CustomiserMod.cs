c7fef09d1e5d2ed0a7f1e74662f7b4c6e46312ae
OfficialPiAddict <mikagoetze@hotmail.com>
2017-10-08 14:25:58 +1030

    Added most of the menu for the doggo customiser. (There still needs to be layout work)

--- DoggoCustomiser/CustomiserMod.cs
+++ DoggoCustomiser/CustomiserMod.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
 using System.Diagnostics.PerformanceData;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Security.Cryptography;
+using DoggoCustomiser.Menus;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -10,6 +12,7 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.Characters;
 
 
 namespace DoggoCustomiser
@@ -18,7 +21,14 @@ namespace DoggoCustomiser
     {
         private ModConfig config;
         private IModHelper helper;
-        
+
+        private Color coatColor;
+        private Color collarColor;
+
+        private static CustomiserMod _instance;
+
+        public static CustomiserMod Instance => _instance ?? (_instance = new CustomiserMod());
+
         public bool CanEdit<T>(IAssetInfo asset)
         {
             return asset.AssetNameEquals(@"Animals\dog");
@@ -29,13 +39,14 @@ namespace DoggoCustomiser
             return Vector3.Distance(a.ToVector3(), b.ToVector3());
         }
 
-        private void FindAndLerpColorWithShading(ref Color[] pixels, Color originalColor, Color targetColor, Color featureColor, float colorTolerance, float shadingMultiplier)
+        private void FindAndLerpColorWithShading(ref Color[] pixels, Color originalColor, Color targetColor,
+            Color featureColor, float colorTolerance, float shadingMultiplier)
         {
             for (int i = 0; i < pixels.Length; i++)
             {
                 //Don't try and recolor transparency.
                 if (pixels[i].A < 255) continue;
-                
+
                 var distance = ColorDistance(pixels[i], originalColor);
                 if (distance < colorTolerance)
                 {
@@ -44,13 +55,14 @@ namespace DoggoCustomiser
             }
         }
 
-        private void FindAndLerpColor(ref Color[] pixels, Color originalColor, Color targetColor, float colorTolerance, bool ignoreAlpha = true)
+        private void FindAndLerpColor(ref Color[] pixels, Color originalColor, Color targetColor, float colorTolerance,
+            bool ignoreAlpha = true)
         {
             for (int i = 0; i < pixels.Length; i++)
             {
-                if ( ignoreAlpha && pixels[i].A < 255) continue;
-                
-                
+                if (ignoreAlpha && pixels[i].A < 255) continue;
+
+
                 var distance = ColorDistance(pixels[i], originalColor);
                 if (pixels[i] == new Color(1, 1, 1, 1))
                 {
@@ -61,63 +73,126 @@ namespace DoggoCustomiser
                     pixels[i] = targetColor;
                 }
             }
+
+
         }
-        
-        
+
+        private void ReadConfig()
+        {
+            config = helper.ReadConfig<ModConfig>();
+            collarColor = config.CollarColor;
+            coatColor = config.CoatColor;
+        }
+
+
+
         public void Edit<T>(IAssetData asset)
         {
             this.Monitor.Log("Processing dog sprite.");
-            
+
             //Colors of the original dog.
             Color collarColor = new Color(97, 141, 248);
             Color litCoatColor = new Color(255, 198, 0);
             Color tongueColor = new Color(255, 81, 0);
-            
+
             //Colors for use throughout modification
             Color neutralFeatureColor = new Color(60, 60, 60);
-            
+
             //Lets get the actual dog texture. 
             Texture2D dogTexture2D = asset.AsImage().Data;
-            
-            //Let's read the intended t arget colors from the config file.
-            Color targetCoatColor = config.CoatColor;
-            Color targetCollarColor = config.CollarColor;
-            
+
             //Hardcoded values that make everything work.
-            float coatTolerance = 0.8f;
+            float coatTolerance = 0.7f;
             float collarTolerance = 0.5f;
             float shadingMultiplier = 1.0f;
 
             float tongueTolerance = 0.1f;
-               
+
             //Lets lerp the feature color (gray) towards the target color - to make correct shading
-            neutralFeatureColor = Color.Lerp(neutralFeatureColor, targetCoatColor, 0.02f);
-            
+            neutralFeatureColor = Color.Lerp(neutralFeatureColor, coatColor, 0.02f);
+
             //Alright - lets see what we can do to it here.
             Color[] pixels = new Color[dogTexture2D.Height * dogTexture2D.Width];
             dogTexture2D.GetData(pixels);
-                
+
             //DO NOT CHANGE THE ORDER OF THESE OPERATIONS. THEY MATTER!
-            
+
             //First up lets find the tongue and set it to a specific color for the sake of preserving it.
+            FindAndLerpColor(ref pixels, collarColor, collarColor, collarTolerance);
             FindAndLerpColor(ref pixels, tongueColor, new Color(25, 25, 25, 25), tongueTolerance);
-            FindAndLerpColor(ref pixels, collarColor, targetCollarColor, collarTolerance);
-            FindAndLerpColorWithShading(ref pixels, litCoatColor, targetCoatColor, neutralFeatureColor, coatTolerance, shadingMultiplier);
+            FindAndLerpColorWithShading(ref pixels, litCoatColor, coatColor, neutralFeatureColor, coatTolerance,
+                shadingMultiplier);
             FindAndLerpColor(ref pixels, new Color(25, 25, 25, 25), tongueColor, 0.01f, false);
-            
+
             dogTexture2D.SetData(pixels);
-            
+
             //At this point we would a patch in the new ears textures. And overlay them.
             asset.AsImage().PatchImage(dogTexture2D);
         }
 
+        void ButtonDown(object sender, EventArgsInput e)
+        {
+            if (e.Button == SButton.P)
+            {
+                //This should reload the dog? Hopefully.
+                ResetDog();
+            }
+            else if (e.Button == SButton.L)
+            {
+                //Lets show the menu
+                if (Game1.activeClickableMenu == null)
+                {
+                    Game1.activeClickableMenu = new CustomiseMenu();
+                }
+
+            }
+        }
+
+        public void ChangeCoatColor(Color color)
+        {
+            coatColor = color;
+        }
+
+        public void ChangeCollarColor(Color color)
+        {
+            collarColor = color;
+        }
+
+        public Dog GetDog()
+        {
+            for (var index = 0; index < Game1.currentLocation.characters.Count; index++)
+            {
+                NPC character = Game1.currentLocation.characters[index];
+                if (character.GetType() == typeof(Dog))
+                {
+                    return (Dog) character;
+                }
+            }
+            
+            //Otherwise, lets add the dog to the character list:
+            Dog dog = new Dog(Game1.player.getTileX(), Game1.player.getTileY());
+            Game1.currentLocation.characters.Add(dog);
+            return dog;
+        } 
+
+        public void ResetDog()
+        {
+            config = helper.ReadConfig<ModConfig>();
+            helper.Content.InvalidateCache("Animals/dog.xnb");
+
+            Game1.currentLocation.characters.Remove(GetDog());
+            
+            Game1.currentLocation.addCharacter((NPC)new Dog(Game1.player.getTileX() + 1, Game1.player.getTileY() + 1));           
+        }
 
         
         public override void Entry(IModHelper helper)
         {
-            config = helper.ReadConfig<ModConfig>();
+            _instance = this;
             this.helper = helper;
             this.Monitor.Log("Loaded DoggoCustomiser configuration file.");
+            InputEvents.ButtonPressed += ButtonDown;
+            ReadConfig();
         }
     }
 }
--- DoggoCustomiser/Menus/CustomiseMenu.cs
+++ DoggoCustomiser/Menus/CustomiseMenu.cs
@@ -2,28 +2,123 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
+using StardewValley.Characters;
 using StardewValley.Menus;
 
 namespace DoggoCustomiser.Menus
 {
     public class CustomiseMenu : IClickableMenu
     {
-        private ClickableComponent coatColorPicker, collarColorPicker;
+        private ColorPicker coatColorPicker, collarColorPicker;
 
+        private ClickableTextureComponent okButton;
+        private ClickableTextureComponent previewButton;
 
-        public CustomiseMenu()
+        private ClickableComponent coatLabel, collarLabel, previewLabel;
+        private Rectangle backgroundRectangle;
+        private Vector2 dogPos;
+        
+            
+        private void setUpPositions()
         {
-            Vector2 centeringOnScreen = Utility.getTopLeftPositionForCenteringOnScreen(950, 700, 0, 0);
+            coatLabel = new ClickableComponent(
+                new Rectangle(this.xPositionOnScreen + IClickableMenu.spaceToClearSideBorder + Game1.tileSize, this.yPositionOnScreen + IClickableMenu.spaceToClearTopBorder + Game1.tileSize / 4, Game1.tileSize * 3, Game1.tileSize),
+                "coatLabel", "Coat Color");
+           
+            collarLabel = new ClickableComponent(new Rectangle(this.xPositionOnScreen + IClickableMenu.spaceToClearSideBorder +  (this.width / 2) - ((int) (1.5f * Game1.tileSize)), this.yPositionOnScreen + IClickableMenu.spaceToClearTopBorder + Game1.tileSize / 4, Game1.tileSize * 3, Game1.tileSize),
+                "collarLabel", "Collar Color");
+
+            previewLabel = new ClickableComponent(new Rectangle(this.xPositionOnScreen + this.width - IClickableMenu.spaceToClearSideBorder - IClickableMenu.borderWidth - Game1.tileSize * 3, this.yPositionOnScreen + IClickableMenu.spaceToClearTopBorder + Game1.tileSize / 4, Game1.tileSize * 3, Game1.tileSize), "previewLabel", "Preview" );
+            
+            backgroundRectangle = new Rectangle(
+                this.xPositionOnScreen + this.width - IClickableMenu.spaceToClearSideBorder -
+                IClickableMenu.borderWidth - Game1.tileSize * 3,
+                previewLabel.bounds.Y + Game1.tileSize, Game1.tileSize * 3, Game1.tileSize * 3);
+            
+            
+            dogPos = new Vector2(this.xPositionOnScreen + this.width - IClickableMenu.spaceToClearSideBorder - IClickableMenu.borderWidth - Game1.tileSize * 1.5f, backgroundRectangle.Y + Game1.tileSize * 1.5f);
+            
+            //We want the coatColorPicker all the way on the left. - And everything centered vertically (with a small offset).
+            coatColorPicker = new ColorPicker(coatLabel.bounds.X, coatLabel.bounds.Y + (int) (Game1.tileSize * 1.5f));
+            collarColorPicker = new ColorPicker(collarLabel.bounds.X, collarLabel.bounds.Y + (int) (Game1.tileSize * 1.5f));
+           
+            okButton = new ClickableTextureComponent("OK",
+                new Rectangle(
+                    this.xPositionOnScreen + (this.width / 2) - IClickableMenu.borderWidth -
+                    IClickableMenu.spaceToClearSideBorder - Game1.tileSize,
+                    this.yPositionOnScreen + this.height - IClickableMenu.borderWidth -
+                    IClickableMenu.spaceToClearTopBorder + Game1.tileSize / 4, Game1.tileSize, Game1.tileSize),
+                (string) null, (string) null, Game1.mouseCursors,
+                Game1.getSourceRectForStandardTileSheet(Game1.mouseCursors, 46, -1, -1), 1f, false);
+            
+            previewButton = new ClickableTextureComponent("Preview",
+                new Rectangle(
+                    this.xPositionOnScreen + this.width - IClickableMenu.borderWidth -
+                    IClickableMenu.spaceToClearSideBorder - Game1.tileSize * 3,
+                    this.yPositionOnScreen + this.height - IClickableMenu.borderWidth -
+                    IClickableMenu.spaceToClearTopBorder + Game1.tileSize / 4, Game1.tileSize * 3, Game1.tileSize),
+                (string) null, (string) null, Game1.mouseCursors,
+                Game1.getSourceRectForStandardTileSheet(Game1.mouseCursors, 46, -1, -1), 1f, false);
+            
         }
+
+        public CustomiseMenu() : base(Game1.viewport.Width / 2 - (632 + IClickableMenu.borderWidth * 2) / 2,
+            Game1.viewport.Height / 2 - (300 + IClickableMenu.borderWidth * 2) / 2 - Game1.tileSize,
+            632 + IClickableMenu.borderWidth * 2, 300 + IClickableMenu.borderWidth * 2 + Game1.tileSize, false)
+        {
+            setUpPositions();
+            
+        }
+
+        private void DrawLabel(ClickableComponent label, SpriteBatch b)
+        {
+            Utility.drawTextWithShadow(b, label.label, Game1.smallFont, new Vector2(label.bounds.X, label.bounds.Y), Color.DarkSlateGray); 
+        } 
+    
 
         public override void draw(SpriteBatch b)
         {
-            Vector2 centeringOnScreen = Utility.getTopLeftPositionForCenteringOnScreen(950, 700, 0, 0);
+            Game1.drawDialogueBox(this.xPositionOnScreen, this.yPositionOnScreen, this.width, this.height, false, true, (string) null, false);
+            //This draws the daybackground over which we can show the dog (while you customise it).
+            this.okButton.draw(b, Color.White, 0.75f);
+            this.previewButton.draw(b, Color.White, 0.75f);
+            this.coatColorPicker.draw(b);
+            this.collarColorPicker.draw(b);
+            DrawLabel(this.coatLabel, b); 
+            DrawLabel(this.collarLabel, b);
+            DrawLabel(this.previewLabel, b);
+           
+            b.Draw(Game1.daybg, backgroundRectangle, Color.White);
+            
+            Dog dog = CustomiserMod.Instance.GetDog();
+            //Now lets set up the dog's position so that he renders in the preview field.
+            dog.position = dogPos;
+            dog.draw(b); 
+            this.drawMouse(b);
         }
-        
+
+        public override void leftClickHeld(int x, int y)
+        {
+            base.leftClickHeld(x, y);
+            if (coatColorPicker.containsPoint(x, y)) CustomiserMod.Instance.ChangeCoatColor(coatColorPicker.clickHeld(x, y));
+            if(collarColorPicker.containsPoint(x, y)) CustomiserMod.Instance.ChangeCollarColor(collarColorPicker.click(x, y));
+        }
+
+        public override void receiveLeftClick(int x, int y, bool playSound = true)
+        {
+            base.receiveLeftClick(x, y, playSound);
+            if (okButton.containsPoint(x, y))
+            {
+                //Then we have clicked the okay button, so lets reset the dog, and close the menu.
+                CustomiserMod.Instance.ResetDog();
+                Game1.activeClickableMenu = null;
+            }
+            if (coatColorPicker.containsPoint(x, y)) CustomiserMod.Instance.ChangeCoatColor(coatColorPicker.click(x, y));
+            if(collarColorPicker.containsPoint(x, y)) CustomiserMod.Instance.ChangeCollarColor(collarColorPicker.click(x, y));
+        }
+
         public override void receiveRightClick(int x, int y, bool playSound = true)
         {
-            throw new System.NotImplementedException();
         }
     }
 }
